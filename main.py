a6093246e910803d9b4da92c615e6a9671e70ccf
MelodyAyane <1248286484@qq.com>
2024-01-28 12:56:30 +0800

    Optimise code performance

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ import ttkbootstrap
 import sc2reader
 import os
 import sys
+from collections import defaultdict
 
 class TextRedirector(object):
+    """Redirects standard output to a tkinter text widget."""
     def __init__(self, widget):
         self.widget = widget
 
@@ -15,16 +17,68 @@ class TextRedirector(object):
         self.widget.see(tk.END)
 
     def flush(self):
-        pass  # Add this method to avoid the AttributeError
+        pass  # Required for file-like objects
 
 def open_url(event):
+    """Opens a URL in the default web browser."""
     webbrowser.open_new(event.widget.cget("text"))
 
 def open_url(player_name):
+    """Opens a URL in the default web browser."""
     webbrowser.open_new(f"https://nonapa.com/search?query={player_name}")
 
+def handle_event(event, build_order, current_supply, player):
+    """Handles a single event from the replay."""
+    if isinstance(event, sc2reader.events.tracker.PlayerStatsEvent) and event.pid == player.pid:
+        current_supply = event.food_used  # Update the current supply count
+    elif isinstance(event, sc2reader.events.tracker.UnitBornEvent) and event.control_pid == player.pid:
+        if event.second not in build_order:  # Check if this second is already in the build order
+            build_order[event.second] = {"population": 0, "units": [], "buildings": [], "upgrades": []}  # If not, initialize it
+        if event.second == 0:  # If this is the 0 second time point
+            if 'SCV' in build_order[event.second]["units"]:  # If an SCV has already been added, skip this loop
+                return build_order, current_supply
+            else:  # If no SCV has been added yet, add one and update the population count
+                build_order[event.second]["units"].append(event.unit_type_name)
+                current_supply += 1  # Increase the worker count
+                build_order[event.second]["population"] = current_supply  # Update the population count in the build order
+                return build_order, current_supply
+        build_order[event.second]["units"].append(event.unit_type_name)
+        if event.unit_type_name in ['SCV', 'Probe', 'Drone']:  # Check if the unit is a worker
+            current_supply += 1  # Increase the worker count
+        build_order[event.second]["population"] = current_supply  # Update the population count in the build order
+    elif isinstance(event, sc2reader.events.tracker.UpgradeCompleteEvent) and event.pid == player.pid and event.second != 0:
+        build_order[event.second]["upgrades"].append(event.upgrade_type_name)
+        build_order[event.second]["population"] = current_supply  # Update the population count in the build order
+    elif isinstance(event, sc2reader.events.tracker.UnitInitEvent) and event.control_pid == player.pid and event.second != 0:
+        build_order[event.second]["buildings"].append(event.unit_type_name)
+        build_order[event.second]["population"] = current_supply  # Update the population count in the build order
+
+    return build_order, current_supply
+
+def print_build_order(text_widgets, replay, player, build_order):
+    """Prints the build order for a single player."""
+    text_widgets['Build Order'].tag_configure("bold_large", font=('Microsoft YaHei', 14, 'bold'))
+    text_widgets['Build Order'].insert(tk.END, f"Player {player.name}'s build order:\n", "bold_large")
+    for second in sorted(build_order.keys()):
+        if second == 0:  # Skip the 0 second time point
+            continue
+        if second == min(build_order.keys()):  # Special case for the first time point
+            text_widgets['Build Order'].insert(tk.END,
+                                               f"At {second}s (Population: {build_order[second]['population']}):\n")
+        elif build_order[second]["upgrades"] or build_order[second][
+            "buildings"]:  # Only print the population count if there are upgrades or buildings
+            text_widgets['Build Order'].insert(tk.END,
+                                               f"At {second}s (Population: {build_order[second]['population']}):\n")
+        for unit in build_order[second]["units"]:
+            text_widgets['Build Order'].insert(tk.END, f"    {unit} was built\n")
+        for building in build_order[second]["buildings"]:
+            text_widgets['Build Order'].insert(tk.END, f"    {building} was built\n")
+        for upgrade in build_order[second]["upgrades"]:
+            text_widgets['Build Order'].insert(tk.END, f"    {upgrade} was completed\n")
+        text_widgets['Build Order'].insert(tk.END, "\n")  # Add an empty line after each player's build order
 
 def analyze_replay():
+    """Analyzes a StarCraft II replay."""
     root = tk.Tk()  # Create a standard Tk instance
 
     def close_window():
@@ -79,10 +133,6 @@ def analyze_replay():
 
     # Print game version and map information
     print(f"Game version: {replay.release_string}")
-    if replay.map is not None:
-        print(f"Map size: {replay.map.size}")
-    else:
-        print("Map size: Unknown")
 
     sys.stdout = TextRedirector(text_widgets['Player Info'])
     players_info = []
@@ -99,44 +149,18 @@ def analyze_replay():
     print(f"Players:\n{players_info}")
 
     # Print build order
-    text_widgets['Build Order'].tag_configure("bold_large", font=('Microsoft YaHei', 14, 'bold'))
     for player in replay.players:
-        build_order = {}  # A dictionary to store the build order for each time point
+        build_order = defaultdict(lambda: {"population": 0, "units": [], "buildings": [], "upgrades": []})  # A dictionary to store the build order for each time point
         current_supply = 0  # Variable to store the current supply count
-        for event in replay.events:
-            if isinstance(event, sc2reader.events.tracker.PlayerStatsEvent) and event.pid == player.pid:
-                current_supply = event.food_used  # Update the current supply count
-            elif isinstance(event,
-                            sc2reader.events.tracker.UnitBornEvent) and event.control_pid == player.pid and event.second != 0:
-                if event.second not in build_order:
-                    build_order[event.second] = {"population": current_supply, "units": [], "buildings": [],
-                                                 "upgrades": []}
-                build_order[event.second]["units"].append(event.unit_type_name)
-            elif isinstance(event,
-                            sc2reader.events.tracker.UpgradeCompleteEvent) and event.pid == player.pid and event.second != 0:
-                if event.second not in build_order:
-                    build_order[event.second] = {"population": current_supply, "units": [], "buildings": [],
-                                                 "upgrades": []}
-                build_order[event.second]["upgrades"].append(event.upgrade_type_name)
-            elif isinstance(event,
-                            sc2reader.events.tracker.UnitInitEvent) and event.control_pid == player.pid and event.second != 0:
-                if event.second not in build_order:
-                    build_order[event.second] = {"population": current_supply, "units": [], "buildings": [],
-                                                 "upgrades": []}
-                build_order[event.second]["buildings"].append(event.unit_type_name)
 
-        # Print the build order
-        text_widgets['Build Order'].insert(tk.END, f"Player {player.name}'s build order:\n", "bold_large")
-        for second in sorted(build_order.keys()):
-            text_widgets['Build Order'].insert(tk.END,
-                                               f"At {second}s (Population: {build_order[second]['population']}):\n")
-            for unit in build_order[second]["units"]:
-                text_widgets['Build Order'].insert(tk.END, f"    {unit} was built\n")
-            for building in build_order[second]["buildings"]:
-                text_widgets['Build Order'].insert(tk.END, f"    {building} was built\n")
-            for upgrade in build_order[second]["upgrades"]:
-                text_widgets['Build Order'].insert(tk.END, f"    {upgrade} was completed\n")
-        text_widgets['Build Order'].insert(tk.END, "\n")  # Add an empty line after each player's build order
+        # Add an entry for the 0 second time point
+        build_order[0]["units"].append('SCV')
+        build_order[0]["population"] = 1
+
+        for event in replay.events:
+            build_order, current_supply = handle_event(event, build_order, current_supply, player)
+
+        print_build_order(text_widgets, replay, player, build_order)
 
     # Print game timeline
     sys.stdout = TextRedirector(text_widgets['Game Timeline'])
@@ -161,7 +185,6 @@ def analyze_replay():
             print(f"{message.player.name}: {message.text}")
     else:
         print("No chat messages in this replay.")
-
     # Print metadata
     sys.stdout = TextRedirector(text_widgets['Basic Info'])
     print(f"Replay created at: {replay.date}")
